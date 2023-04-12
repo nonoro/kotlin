7ff27c83427e9a4b4ca66a333be50f303281c0af
nonoro <kgu1022@naver.com>
2023-04-13 01:42:07 +0900

    클래스 변경사항

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -29,4 +29,12 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/lec07/Lec07Main.java
+++ src/main/java/lec07/Lec07Main.java
@@ -3,15 +3,12 @@ package lec07;
 import domain.JavaFilePrinter;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Lec07Main {
     public static void main(String[] args) throws IOException {
         JavaFilePrinter printer = new JavaFilePrinter();
-        printer.readFile();
+        printer.readFileV1();
     }
 
     private int parseIntOrThrowV1(@NotNull String str) {
