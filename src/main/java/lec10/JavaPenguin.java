6c3c5b664a61cc8da7b0c8c9e1198646d2400a24
nonoro <kgu1022@naver.com>
2023-04-19 11:04:08 +0900

    Lec 10. 코틀린에서 상속을 다루는 방법

---
+++ src/main/java/lec10/JavaPenguin.java
@@ -0,0 +1,26 @@
+package lec10;
+
+public class JavaPenguin extends JavaAnimal implements JavaFlyable, JavaSwimable{
+
+    private final int wingCount;
+
+    public JavaPenguin(String species) {
+        super(species, 2);
+        this.wingCount = 2;
+    }
+
+    @Override
+    public void move() {
+        System.out.println("펭귄이 움직입니다! 꽥꽥");
+    }
+
+    public int getWingCount() {
+        return super.legCount + this.wingCount;
+    }
+
+    @Override
+    public void act() {
+        JavaSwimable.super.act();
+        JavaFlyable.super.act();
+    }
+}
